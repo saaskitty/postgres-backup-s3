9f74a0ac0de61b7d6a6fe6c3432def063e6c242c
saaskitten <founder@saaskitty.com>
2024-07-20 01:02:55 +0800

    Update backup.sh

--- src/backup.sh
+++ src/backup.sh
@@ -5,17 +5,14 @@ set -o pipefail
 
 source ./env.sh
 
-echo "Creating backup of $POSTGRES_DATABASE database..."
+echo "Creating database backup..."
 pg_dump --format=custom \
-        -h $POSTGRES_HOST \
-        -p $POSTGRES_PORT \
-        -U $POSTGRES_USER \
-        -d $POSTGRES_DATABASE \
+        --dbname=${POSTGRES_DB_URL} \
         $PGDUMP_EXTRA_OPTS \
         > db.dump
 
 timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DB}_${timestamp}.dump"
 
 if [ -n "$PASSPHRASE" ]; then
   echo "Encrypting backup..."
