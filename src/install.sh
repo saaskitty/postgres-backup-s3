ab2a5b0503d1d3441dce747fb6ed299bc6c72d4a
saaskitten <founder@saaskitty.com>
2024-07-22 16:17:34 +0800

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -3,14 +3,19 @@
 set -eux
 set -o pipefail
 
+apk add build-base gcc postgresql-dev bison flex perl readline-dev zlib-dev
 apk update
 
 # install pg_dump
-apk add postgresql-client
+wget https://ftp.postgresql.org/pub/source/v17beta2/postgresql-${POSTGRES_VERSION}.tar.gz
+tar -xvzf postgresql-${POSTGRES_VERSION}.tar.gz
+cd postgresql-${POSTGRES_VERSION}
+./configure
+make
+make install
 
 # install gpg
 apk add gnupg
-
 apk add aws-cli
 
 # install go-cron
@@ -21,7 +26,6 @@ rm go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
 mv go-cron /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
-
 
 # cleanup
 rm -rf /var/cache/apk/*
